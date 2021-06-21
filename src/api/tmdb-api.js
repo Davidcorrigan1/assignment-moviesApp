ac7962d6c68b30c00a08915784ef9126ed1bd4e9
David Corrigan <davidcorrigan1@gmail.com>
2021-06-21 18:51:18 +0100

    Show movie review excerpts; Refactor Details page

--- src/api/tmdb-api.js
+++ src/api/tmdb-api.js
@@ -22,6 +22,16 @@ export const getMovies = () => {
       .then(json => json.genres);
   };
   
+  export const getMovieReviews = (id) => {
+    return fetch(
+      `https://api.themoviedb.org/3/movie/${id}/reviews?api_key=${process.env.REACT_APP_TMDB_KEY}`
+    )
+      .then((res) => res.json())
+      .then((json) => {
+        // console.log(json.results);
+        return json.results;
+      });
+  };
   export const getMovieImages = (id) => {
     return fetch(
       `https://api.themoviedb.org/3/movie/${id}/images?api_key=${process.env.REACT_APP_TMDB_KEY}`
