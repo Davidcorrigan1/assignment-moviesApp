3e8e1482410931d7dce576c123edda1af8d11cbf
David Corrigan <davidcorrigan1@gmail.com>
2021-08-10 20:47:14 +0100

    Added new StoryBook story for MovieCardSearch component

---
+++ src/stories/movieCardSearch.stories.js
@@ -0,0 +1,39 @@
+import React from "react";
+import MovieCardSearch from "../components/movieCardSearch";
+import SampleMovie from "./sampleData";
+import { MemoryRouter } from "react-router";
+import MoviesContextProvider from "../contexts/moviesContext";
+import { action } from "@storybook/addon-actions";
+import AddToFavoritesIcon from "../components/cardIcons/addToFavorites";
+
+export default {
+  title: "Home Page/MovieCardSearch",
+  component: MovieCardSearch,
+  decorators: [
+    (Story) => <MemoryRouter initialEntries={["/"]}>{Story()}</MemoryRouter>,
+    (Story) => <MoviesContextProvider>{Story()}</MoviesContextProvider>,
+  ],
+};
+
+export const Basic = () => {
+  return (
+    <MovieCardSearch
+      movie={SampleMovie}
+      action={(movie) => <AddToFavoritesIcon movie={movie} />}
+      taging={(movie) => null}
+    />
+  );
+};
+Basic.storyName = "Default";
+
+export const Exceptional = () => {
+  const sampleNoPoster = { ...SampleMovie, poster_path: undefined };
+  return (
+    <MovieCardSearch
+      movie={sampleNoPoster}
+      action={(movie) => <AddToFavoritesIcon movie={movie} />}
+      taging={(movie) => null}
+    />
+  );
+};
+Exceptional.storyName = "exception";
